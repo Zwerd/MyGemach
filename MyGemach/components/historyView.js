df967cbc2da61a9ecdfa9304f90a522a491779b8
zwerd <guy.zwerdling@gmail.com>
2019-02-04 17:10:18 +0200

    update history components

---
+++ MyGemach/components/historyView.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {Alert, Dimensions, Image, TouchableOpacity, TouchableNativeFeedback, ScrollView, ImageBackground, StyleSheet, Text, View, StatusBar, TextInput, BackHandler} from 'react-native';
+
+
+export default class HistoryView extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    backgroundColor:this.props.backgroundColor,
+    };
+}
+
+renderList(this.props.historiesData){
+  return this.props.historiesData.map(data =>
+    <View style={styles.View}>
+      <Text>שם: {this.props.name}</Text>
+      <Text>תיאור: {this.props.description}</Text>
+      <Text>תאריך: {this.props.date}</Text>
+      <Text>מספר: {this.props.itemNumber+1}</Text>
+    </View>
+
+render(){
+  return(
+    {
+      this.props.historiesData.map(data =>
+        <View style={styles.View}>
+          <Text>שם: {data.fullName}</Text>
+          <Text>כתובת: {data.address}</Text>
+          <Text>מספר: {data.phone}</Text>
+        </View>
+        <View style={styles.View}>
+          <Text>תאריך מסירה: {data.deliverDate}</Text>
+          <Text>תאריך החזרה: {data.reciverDate}</Text>
+        </View>
+    }
+    )
+  }
+}
+const styles = StyleSheet.create({
+  display:{
+    justifyContent: 'center',
+    textAlign: 'right',
+    margin: 5,
+    marginRight:null,
+    marginBottom:0,
+    borderWidth:1,
+    borderColor:'#00B0F0',
+  },
+  View:{
+    height: dim.height/8,
+    width: ((dim.width-11)/4) * 3,
+    borderBottomWidth:1,
+    padding:2,
+  },
+  ViewImage:{
+    width: (dim.width-16)/4,
+    overflow: 'hidden',
+  },
+  previewImage: {
+    width: "100%",
+    height: "100%",
+  }
+});
+
+export default Card
