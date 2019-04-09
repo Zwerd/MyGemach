50a303bdc6dca677765749d07aaf80c868aef814
zwerd <guy.zwerdling@gmail.com>
2019-04-09 07:38:16 +0300

    check on setting if the language props pass to setting screen and if the value of language will change

--- backupcopy/MyGemach/components/settings.js
+++ backupcopy/MyGemach/components/settings.js
@@ -69,11 +69,11 @@ setLanguage(value){
         <ScrollView style={{backgroundColor:'rgba(255, 255, 255, 0.8)',height:dim.height-barHeight}}>
           <View style={{borderBottomWidth:10, borderColor:'#DCDCDC'}}>
             <View style={{margin:5, marginTop:0, borderBottomWidth:1,borderColor:'#DCDCDC',justifyContent:'center'}}>
-              <Text style={{fontSize:barHeight/2}}>{this.state.language.settings.language.title}</Text>
+              <Text style={{fontSize:barHeight/2}}>{this.props.language.settings.language.title}</Text>
 
               <Menu renderer={renderers.NotAnimatedContextMenu} onSelect={value => this.setLanguage(value)}>
                  <MenuTrigger>
-                   <Text style={{fontSize:barHeight/2.5}}>{this.state.language.settings.language.value}</Text>
+                   <Text style={{fontSize:barHeight/2.5}}>{this.props.language.settings.language.value}</Text>
                  </MenuTrigger>
                  <MenuOptions >
                    <MenuOption value={'heb'} style={{margin:2}}>
